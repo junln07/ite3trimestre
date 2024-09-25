acaf6393f6306b6cc2a304f8760ded57974463d6
junln07 <161731188+junln07@users.noreply.github.com>
2024-09-25 09:42:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container')
-    let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+    const container = document.getElementById('container');
+    const cartao = document.createElement('article');
+    cartao.className = 'cartao';
 
     cartao.innerHTML = `
-    <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
-    <div class="cartao__conteudo__pergunta">
-        <p>${pergunta}</p>
-    </div>
-    <div class="cartao__conteudo__resposta">
-        <p>${resposta}</p>
-    </div>
-    </div>
-    `
+        <div class="cartao__conteudo">
+            <h3>${categoria}</h3>
+            <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+            </div>
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+            </div>
+        </div>
+    `;
 
-    let respostaEstaVisivel = false
+    cartao.addEventListener('click', () => {
+        cartao.classList.toggle('active');
+    });
 
-    function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel
-        cartao.classList.toggle('active', respostaEstaVisivel)
-    }
-    cartao.addEventListener('click', viraCartao)
-
-
-    container.appendChild(cartao)
-
+    container.appendChild(cartao);
 }
+
+// Criação dos cartões
+const flashcards = [
+    {
+        categoria: 'Programação',
+        pergunta: 'O que é Python?',
+        resposta: 'O Python é uma linguagem de programação'
+    },
+    {
+        categoria: 'Geografia',
+        pergunta: 'Qual a capital da França?',
+        resposta: 'A capital da França é Paris'
+    },
+    {
+        categoria: 'Programação',
+        pergunta: 'O que é uma função?',
+        resposta: 'Uma função é um bloco de código que executa alguma tarefa'
+    },
+    {
+        categoria: 'Lingua inglesa',
+        pergunta: 'Como se diz oi em Inglês?',
+        resposta: 'Oi em inglês é HI (RAI)'
+    }
+];
+
+// Chamada da função para criar os cartões
+flashcards.forEach(cartao => {
+    criaCartao(cartao.categoria, cartao.pergunta, cartao.resposta);
+});
